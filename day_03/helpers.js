3a013700bbc4f9fa65b7c3a677b55b478dc8f585
Gianluca <gcinellu@gmail.com>
2023-12-03 22:00:20 +0100

    day3

--- day_03/helpers.js
+++ day_03/helpers.js
@@ -1,83 +1,4 @@
 const {sum, product} = require("../common/helpers");
-const findNumbersInString = (string) => {
-  const regex = new RegExp(/\d+/g)
-  const numbers = string.match(regex)
-  return numbers?.map(number => parseInt(number)) ?? []
-}
-
-const findIndexStartOfNumber = (string, number) => {
-  const regex = new RegExp(`(?<!\\d)${number}(?!\\d)`);
-  return string.search(regex);
-}
-
-function findIndexEndOfNumber(string, number) {
-  const numberString = number.toString();
-  const regex = new RegExp(`(?<!\\d)${number}(?!\\d)`);
-
-  const indexEnd = string.search(regex);
-  if (indexEnd === -1) {
-    return -1;
-  }
-
-  return indexEnd + numberString.length - 1;
-}
-
-const isSymbol = (character) => {
-  return !!character.replaceAll('.', '').match(/\W/);
-}
-
-const checkIfNumberIsHorizontallyAdjacentToSymbol = (fullString, inputNumber, indexStart, indexEnd, rowIndex) => {
-  const characterBefore = fullString[indexStart - 1] ?? '';
-  const characterAfter = fullString[indexEnd + 1] ?? '';
-
-  const isAdjacentToSymbolBefore = characterBefore && isSymbol(characterBefore);
-  const isAdjacentToSymbolAfter = characterAfter && isSymbol(characterAfter);
-
-  return isAdjacentToSymbolBefore || isAdjacentToSymbolAfter;
-}
-
-const isNumberVerticallyAdjacentToSymbol = (matrix, rowIndex, number, indexStart, indexEnd) => {
-  const previousRow = rowIndex > 0 ? matrix[rowIndex - 1] : '';
-  const nextRow = rowIndex < matrix.length - 1 ? matrix[rowIndex + 1] : '';
-
-  const adjustedIndexStart = indexStart === 0 ? 0 : indexStart - 1;
-  const adjustedIndexEnd = indexEnd >= matrix[rowIndex]?.length - 1 ? indexEnd : indexEnd + 2;
-
-  const adjacentTextInPreviousRow = previousRow && previousRow.slice(adjustedIndexStart, adjustedIndexEnd);
-  const adjacentTextInNextRow = nextRow && nextRow?.slice(adjustedIndexStart, adjustedIndexEnd);
-
-  const isAdjacentToSymbolInPreviousRow = !!adjacentTextInPreviousRow && isSymbol(adjacentTextInPreviousRow);
-  const isAdjacentToSymbolInNextRow = !!adjacentTextInNextRow && isSymbol(adjacentTextInNextRow);
-
-  return isAdjacentToSymbolInPreviousRow || isAdjacentToSymbolInNextRow;
-}
-
-const getSolution1 = (data) => {
-  const matrix = data.map(line => line.replaceAll('\\r', ''))
-  const validNumbers = []
-
-  for (let [rowIndex, row] of matrix.entries()) {
-    const numbers = findNumbersInString(row)
-
-
-
-    for (let number of numbers) {
-      const indexStart = findIndexStartOfNumber(row, number)
-      const indexEnd = findIndexEndOfNumber(row, number)
-      if (checkIfNumberIsHorizontallyAdjacentToSymbol(row, number, indexStart, indexEnd, rowIndex)) {
-        validNumbers.push(number)
-        continue;
-      }
-
-      if (isNumberVerticallyAdjacentToSymbol(matrix, rowIndex, number, indexStart, indexEnd)) {
-        validNumbers.push(number)
-        continue
-      }
-    }
-  }
-
-  return sum(validNumbers)
-}
 
 const getSolutions = (data) => {
   const length = data.length;
@@ -93,18 +14,18 @@ const getSolutions = (data) => {
   data.forEach((row, rowIndex) => {
     const matches = [...row.matchAll(/\d+/g)];
 
-    matches.forEach(n => {
-      let edge = [];
+    matches.forEach(match => {
+      let edges = [];
 
       [rowIndex - 1, rowIndex, rowIndex + 1].forEach(rowEdge => {
-        for (let character = n.index - 1; character <= n.index + n[0].length; character++) {
-          edge.push(`${rowEdge},${character}`);
+        for (let character = match.index - 1; character <= match.index + match[0].length; character++) {
+          edges.push(`${rowEdge},${character}`);
         }
       });
 
-      edge.forEach(o => {
-        if (chars.hasOwnProperty(o)) {
-          chars[o].push(parseInt(n[0]));
+      edges.forEach(edge => {
+        if (chars.hasOwnProperty(edge)) {
+          chars[edge].push(parseInt(match[0], 10));
         }
       });
     });
